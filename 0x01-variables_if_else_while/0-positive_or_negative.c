c835c641a654c0f75e2e8692547d5969662f92d7
Miss-Bentil <amabbentil@gmail.com>
2022-07-01 21:47:30 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: prints a random number and evaluates if its positive, negative
- * or zero
- *
  * Return: Always 0
  */
-
 int main(void)
 {
 	int n;
